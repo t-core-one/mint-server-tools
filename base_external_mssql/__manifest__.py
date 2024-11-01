bb0b6021b013b4a41ea84a6588e4d018ba3b5875
Janik von Rotz <login@janikvonrotz.ch>
2024-11-01 10:46:17 +0100

    feat: task lint

--- base_external_mssql/__manifest__.py
+++ base_external_mssql/__manifest__.py
@@ -4,15 +4,12 @@
         Module summary.
     """,
     "author": "Mint System GmbH, Odoo Community Association (OCA)",
-    "website": "https://www.mint-system.ch",
+    "website": "https://github.com/OCA/sale-workflow",
     "category": "Tools",
     "version": "17.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["base"],
-    "data": [
-        "security/ir.model.access.csv",
-        "views/base_external_mssql.xml"
-    ],
+    "data": ["security/ir.model.access.csv", "views/base_external_mssql.xml"],
     "application": False,
     "auto_install": False,
 }
--- base_external_mssql/models/base_external_mssql.py
+++ base_external_mssql/models/base_external_mssql.py
@@ -1,12 +1,10 @@
 import logging
 from contextlib import contextmanager
-import pyodbc
+
 import pymssql
+import pyodbc
 
-
-
-
-from odoo import _, api, fields, models, tools
+from odoo import api, fields, models, tools
 from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
@@ -14,9 +12,10 @@ _logger = logging.getLogger(__name__)
 # this is need otherwise pyodbc will not work correctly
 pyodbc.setDecimalSeparator(".")
 
+
 class BaseExternalMssql(models.Model):
-    _name = 'base.external.mssql'
-    _description = 'Base External Mssql'
+    _name = "base.external.mssql"
+    _description = "Base External Mssql"
 
     name = fields.Char("Name of Datasource", required=True)
     server = fields.Char("Datenbase Server", required=True)
@@ -24,28 +23,36 @@ class BaseExternalMssql(models.Model):
     username = fields.Char("Database User", required=True)
     password = fields.Char("Database Password", required=True)
     connection_string = fields.Text(readonly=True, compute="_compute_connection_string")
-    priority = fields.Boolean(string='Use this DB')
-    driver = fields.Selection([('pyodbc', 'pyodbc'), ('pymssql', 'pymssql')], default='pymssql')
-    
+    priority = fields.Boolean(string="Use this DB")
+    driver = fields.Selection(
+        [("pyodbc", "pyodbc"), ("pymssql", "pymssql")], default="pymssql"
+    )
+
     @api.depends("server", "database", "username", "password")
     def _compute_connection_string(self):
         for record in self:
-            if record.driver == 'pyodbc':
-                conn_string = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={record.server};DATABASE={record.database};UID={record.username};PWD={record.password};TrustServerCertificate=yes;'
-            elif record.driver == 'pymssql':
+            if record.driver == "pyodbc":
+                conn_string = f"DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={record.server};DATABASE={record.database};UID={record.username};PWD={record.password};TrustServerCertificate=yes;"
+            elif record.driver == "pymssql":
                 conn_string = f'server="{record.server}", user="{record.username}", password="{record.password}", database="{record.database}", tds_version="7.0"'
-                
+
             record.connection_string = conn_string
 
-    
     @contextmanager
     def connection_open(self):
         """It provides a context manager for the data source."""
         try:
-            if self.driver == 'pyodbc':
+            if self.driver == "pyodbc":
                 connection = pyodbc.connect(self.connection_string, autocommit=True)
-            elif self.driver == 'pymssql':
-                connection = pymssql.connect(server=self.server, user=self.username, password=self.password, database=self.database, tds_version="7.0", autocommit=True)
+            elif self.driver == "pymssql":
+                connection = pymssql.connect(
+                    server=self.server,
+                    user=self.username,
+                    password=self.password,
+                    database=self.database,
+                    tds_version="7.0",
+                    autocommit=True,
+                )
             yield connection
         finally:
             try:
@@ -64,24 +71,23 @@ class BaseExternalMssql(models.Model):
             else:
                 cur = connection.cursor(as_dict=True)
             cur.execute(query)
-            if query_type == 'insert':
+            if query_type == "insert":
                 # connection.commit() not needed for pymssql
-                #res = cur.execute('SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];') # this works for pyodbc
-                #last_id = res.fetchval() # this works for pyodbc
+                # res = cur.execute('SELECT SCOPE_IDENTITY() AS [SCOPE_IDENTITY];') # this works for pyodbc
+                # last_id = res.fetchval() # this works for pyodbc
                 last_id = cur.lastrowid
                 return last_id
-            elif query_type == 'select':
+            elif query_type == "select":
                 rows = cur.fetchall()
                 return rows
 
-            elif query_type == 'select_one':
+            elif query_type == "select_one":
                 # result = cur.fetchval()
                 # result = cur.fetchone()[0]
                 result = cur.fetchone()
-                _logger.info('RESULT FROM SELECT ONE: %s' % (result,))
+                _logger.info("RESULT FROM SELECT ONE: %s" % (result,))
                 connection.commit()
                 return result
-
 
     def connection_test(self):
         """It tests the connection
@@ -94,36 +100,34 @@ class BaseExternalMssql(models.Model):
                 pass
         except Exception as e:
             raise ValidationError(
-                "Connection test failed:\n" "Here is what we got instead:\n%s"
-                % tools.ustr(e)
+                "Connection test failed:\n"
+                "Here is what we got instead:\n%s" % tools.ustr(e)
             )
         else:
-            rows = self.execute('select', 'SELECT @@VERSION', as_dict=False)
+            rows = self.execute("select", "SELECT @@VERSION", as_dict=False)
             message = "Datenbbank-Version:\n"
             for row in rows:
                 message += f"{row[0]} \n"
-            
+
             return {
-                'type': 'ir.actions.client',
-                'tag': 'display_notification',
-                'params': {
-                    'title': 'Datenbanktest erfolgreich',
-                    'message': message,
-                    'sticky': False,
-                }
+                "type": "ir.actions.client",
+                "tag": "display_notification",
+                "params": {
+                    "title": "Datenbanktest erfolgreich",
+                    "message": message,
+                    "sticky": False,
+                },
             }
 
-    
     def set_priority(self):
         # Ensure only one record is selected
         if len(self) != 1:
             raise ValidationError("Bitte nur eine Datenbank verwenden.")
-        
+
         # Set all records' priority to False
-        self.search([]).write({'priority': False})
-        
+        self.search([]).write({"priority": False})
+
         # Set the selected record's priority to True
-        self.write({'priority': True})
-        
+        self.write({"priority": True})
+
         return True
-        
