fa73b33fcdfad0eba7c30f3164cea0a725aff7b5
Janik von Rotz <login@janikvonrotz.ch>
2024-12-02 12:05:33 +0100

    feat: init branch

---
+++ mail_server_filter/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Mail Server Filter",
+    "summary": """
+        Filter outgoing and incoming mail server by database name.
+    """,
+    "author": "Mint System GmbH, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/sale-workflow",
+    "category": "Technical",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": ["views/ir_mail_server.xml", "views/fetchmail_server.xml"],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+    "images": ["images/screen.png"],
+}
